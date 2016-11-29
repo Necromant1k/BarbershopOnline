59eaeb6b8d7926af3e0d50310b7d1a187b83408e
Andrey Guzyuk <a.guzyuk.prof@gmail.com>
2016-11-30 00:51:08 +0300

    BarberShop Online Service

---
+++ README.md
@@ -0,0 +1,3 @@
+# BarberShop
+[![Build Status](https://secure.travis-ci.org/Necromant1k/BarberShop.png?branch=master)](https://travis-ci.org/Necromant1k/BarberShop)
+[![Coverage Status](https://coveralls.io/repos/Necromant1k/BarberShop/badge.svg?branch=master)](https://coveralls.io/r/Necromant1k/BarberShop/?branch=master)
---
+++ client/dev/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule, FormBuilder } from '@angular/forms';
+import { BrowserModule  } from '@angular/platform-browser';
+import { MaterialModule } from '@angular/material';
+import { TodoCmp }   from './todo/components/todo-cmp';
+import { TodoService }   from './todo/services/todo-service';
+
+@NgModule({
+    imports: [
+      BrowserModule,
+      FormsModule,
+      HttpModule,
+      MaterialModule
+    ],
+   declarations: [
+      TodoCmp,
+    ],
+    providers: [
+      TodoService,
+    ],
+    bootstrap: [
+      TodoCmp,
+    ],
+})
+export class AppModule {}
